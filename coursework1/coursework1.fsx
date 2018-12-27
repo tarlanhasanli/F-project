b6375759a77a5d6eefd96a54c7b7db981360d296
Tarlan Hasanli <tahasa@ttu.ee>
2018-12-27 02:31:44 +0200

    coursework1 initial commit

---
+++ coursework1/coursework1.fsx
@@ -0,0 +1,131 @@
+// 1. Associate an identifier "myFirstList" with an empty list of booleans.
+
+
+
+
+
+// 2. Define a function
+//
+// elem : int -> 'a list -> 'a
+//
+// so that 'elem i xs' evaluates to the i-th element in the list xs.
+//
+// The function should throw an exception when i is less than zero or greater than
+// the number of elements in xs. You should use 'failwith' to throw an exception.
+//
+// The function must be implemented via explicit recursion.
+//
+// Keep in mind that a list can either be empty ([]) or non-empty (x :: xs)
+// and you need to define what is the i-th element of the list in both
+// cases.
+//
+// When you have completed this definition then think about the time complexity
+// of your implementation.
+
+
+
+
+
+
+
+// Here is a type synonym for a tuple for representing information about artists.
+
+type Artist = (string * int * bool * string)
+
+// The components of the tuple are:
+// * name of the artist
+// * year of birth
+// * is the artist alive
+// * country of birth
+
+
+// 3. Define a list
+//
+// artists : Artist list
+//
+// which contains information about your favourite artists.
+// The list should contain at least five unique artists.
+//
+// You can use this list in the following exercises to test your code.
+
+
+
+
+
+
+
+
+
+// 4. Define a function
+//
+// bornLaterThan : int -> Artist list -> Artist list
+//
+// so that 'bornLaterThan y xs' evaluates to
+// all the artists x in xs satisfying the condition (birth year of x is greater than y)
+// and nothing else.
+//
+// The function must preserve the relative ordering of elements in xs.
+//
+// The function must preserve duplicates.
+
+
+
+
+
+
+
+
+
+
+
+// 5. Define a function
+//                        
+// artistsFrom : string -> Artist list -> Artist list
+//                        
+// so that 'artistsFrom c xs' evaluates to
+// all the artists in xs who were born in c
+// and nothing else.
+//
+// The function must preserve the relative ordering of elements in xs.
+//
+// The function must preserve duplicates.
+
+
+
+
+
+
+
+
+
+
+// 6. Define a function
+//                          
+// names : Artist list -> string list
+//                          
+// so that 'names xs' evaluates to the names of the artists in xs.
+//
+// 'xs' and 'names xs' must have the same number of elements.
+//
+// The element at index i in 'names xs' must be the name of the
+// artist at index i in 'xs'.
+
+
+
+
+
+
+
+
+
+
+
+// 7. Using your solutions to previous exercises define a function
+//    
+// areFromAndBornLaterThan : string -> int -> Artist list -> string list
+//    
+// so that 'areFromAndBornLaterThan c y xs' evaluates to the names
+// of the artists in xs who are from c and are born later than y.
+//
+// This should be a one-line definition.
+
