ffc848fa06aa33adc7cfae1620cc9c7b56a08df0
Tarlan Hasanli <tarlanhasanli9@gmail.com>
2019-01-11 03:00:16 +0200

    implementing createDir function for file system

--- Property based testing/FileSystem.fs
+++ Property based testing/FileSystem.fs
@@ -7,11 +7,27 @@ module FileSystem =
         | [] -> [[name]]
         | head :: tail -> (name :: head) :: prepend name tail
 
+    let rec contains (nodeName : string) (fs : FsTree) : bool =
+        match fs with
+        | Node [] -> false
+        | Node ((name, tree) :: rest) when compare name nodeName = 0 -> true
+        | Node (_ :: rest) -> contains nodeName (Node rest) 
+
+    let treeToList (Node n) = n 
+
     let rec show (fs:FsTree) : string list list = 
         match fs with
         | Node [] -> []
         | Node ((name, tree) :: rest) -> prepend name (show tree) @ show (Node rest)
 
-    let rec createDir (p : string list) (fs : FsTree) : FsTree = failwith "not implemented"     
+    let rec createDir (p : string list) (fs : FsTree) : FsTree = 
+        match p, fs with
+        | [], _ -> fs
+        | [nodeName], _ when nodeName = null || nodeName = "" -> failwith "not Wellformed file name: empty or null" 
+        | [nodeName], _ when contains nodeName fs -> fs
+        | [nodeName], Node k -> Node (k @ [nodeName, Node []])
+        | _, Node [] -> fs
+        | hd::tl, Node ((name, tree) :: rest) when hd = name -> Node ((name, (createDir tl tree)) :: rest)
+        | _, Node (k :: rest) -> Node (k :: treeToList (createDir p (Node rest)))
     
     let rec delete (p : string list) (fs : FsTree) = failwith "not implemented"
