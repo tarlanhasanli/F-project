4aac248bb83534c57273040f293f83f66ddc2edc
Tarlan Hasanli <tarlanhasanli9@gmail.com>
2019-01-11 02:59:23 +0200

    implementing show function for file system

--- Property based testing/FileSystem.fs
+++ Property based testing/FileSystem.fs
@@ -2,7 +2,15 @@ type FsTree = Node of (string * FsTree) list
 
 module FileSystem =
 
-    let rec show (fs:FsTree) : string list list = failwith "not implemented"
+    let rec prepend (name:string) (path:string list list) : string list list =
+        match path with
+        | [] -> [[name]]
+        | head :: tail -> (name :: head) :: prepend name tail
+
+    let rec show (fs:FsTree) : string list list = 
+        match fs with
+        | Node [] -> []
+        | Node ((name, tree) :: rest) -> prepend name (show tree) @ show (Node rest)
 
     let rec createDir (p : string list) (fs : FsTree) : FsTree = failwith "not implemented"     
     
